6a43d4a54b6475f7c7e9f6678a06e280000d54ab
irakodes <iracodes.io@gmail.com>
2025-12-25 23:46:39 +0200

    Updates

--- src/main/java/online/erakodes/xml_dsign/XMLDigitalSignatureApplication.java
+++ src/main/java/online/erakodes/xml_dsign/XMLDigitalSignatureApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XmlDsignApplication {
+public class XMLDigitalSignatureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XmlDsignApplication.class, args);
+		SpringApplication.run(XMLDigitalSignatureApplication.class, args);
 	}
 
 }
--- src/test/java/online/erakodes/xml_dsign/TestXmlDsignApplication.java
+++ src/test/java/online/erakodes/xml_dsign/TestXmlDsignApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestXmlDsignApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(XmlDsignApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(XMLDigitalSignatureApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
