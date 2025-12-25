6b1ec2068fa2e860154e9fc433b12866e1d2bc52
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-25 19:35:42 +0200

    Inception

---
+++ src/main/java/online/erakodes/xml_dsign/XmlDsignApplication.java
@@ -0,0 +1,13 @@
+package online.erakodes.xml_dsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlDsignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlDsignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/erakodes/xml_dsign/XmlDsignApplicationTests.java
@@ -0,0 +1,15 @@
+package online.erakodes.xml_dsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class XmlDsignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
