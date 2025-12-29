c07da1f85551a69f714832704577009c46dc6de4
irakodes <iracodes.io@gmail.com>
2025-12-29 02:43:24 +0200

    Add application service integrating ISO 20022 message formatter

--- src/main/java/online/erakodes/xml_dsign/service/MessageHandler.java
+++ src/main/java/online/erakodes/xml_dsign/service/MessageHandler.java
@@ -6,9 +6,11 @@ import online.erakodes.xml_dsign.model.AccountLookupResponse;
 import online.erakodes.xml_dsign.model.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CompletableFuture;
 
+@Component
 public class MessageHandler implements IMessageHandler<AccountLookupDto, AccountLookupResponse> {
 
     private final static Logger log = LoggerFactory.getLogger(MessageHandler.class);
@@ -16,7 +18,7 @@ public class MessageHandler implements IMessageHandler<AccountLookupDto, Account
     @Override
     public CompletableFuture<Result<AccountLookupResponse>> handle(AccountLookupDto request) {
         var message = ISOMessageHandler.formatCAMT00300107(new String[] {
-                request.getAccountId(), request.getAccountName()
+                request.accountId(), request.accountName()
         });
 
         log.info("CAMT.003.001.07 formatted: {}", message);
