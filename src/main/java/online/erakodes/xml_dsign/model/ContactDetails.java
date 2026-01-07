2a7b94e9d75438dac21bdc4237ad452032844b49
irakodes <iracodes.io@gmail.com>
2026-01-07 04:26:11 +0200

    Refactor and add support for legacy ACMT.007.001.02 message formatting
    Refactored `formatACMT00700102` to improve clarity and ensure message ID generation is consistent. Added a new overloaded method for legacy support, allowing input through string arrays for backward compatibility. Introduced `ContactDetails#fromArray` to simplify creating `ContactDetails` from arrays.

--- src/main/java/online/erakodes/xml_dsign/model/ContactDetails.java
+++ src/main/java/online/erakodes/xml_dsign/model/ContactDetails.java
@@ -2,4 +2,7 @@ package online.erakodes.xml_dsign.model;
 
 public record ContactDetails(String name, String mobileNumber, String email, String address) {
 
+    public static ContactDetails fromArray(String[] args) {
+        return new ContactDetails(args[0], args[1], args[2], args[3]);
+    }
 }
